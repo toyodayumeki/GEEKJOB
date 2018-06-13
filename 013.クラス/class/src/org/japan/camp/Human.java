d95a6340981a2b22ce31a7ab41afb5fc04f40325
toyodayumeki <y.toyoda.rkt@gmail.com>
2018-06-13 15:33:36 +0900

    クラス課題１　提出します。

---
+++ 013.クラス/class/src/org/japan/camp/Human.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japan.camp;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Human {
+    
+    public String name = "";
+    public int age = 0;
+    
+     public void setHuman(String name, int suji) {
+        this.name = name;
+        this.age = suji;
+    }
+}
