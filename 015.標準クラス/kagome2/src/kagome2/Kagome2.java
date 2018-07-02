fd7edaa1b42076a2853c540192c22cc7b073aedb
toyodayumeki <y.toyoda.rkt@gmail.com>
2018-07-02 15:50:40 +0900

    標準クラス課題　バイト数と文字数取得　提出します。

---
+++ 015.標準クラス/kagome2/src/kagome2/Kagome2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kagome2;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Kagome2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String be="お腹がすいた";
+        System.out.print(be);
+        
+        
+        
+    }
+    
+}
