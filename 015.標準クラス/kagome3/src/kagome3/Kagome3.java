320ac8bcd8f8eb54dca129a46f5ab953fb228933
toyodayumeki <y.toyoda.rkt@gmail.com>
2018-07-02 16:56:38 +0900

    標準クラス課題　文字の入れ替え

---
+++ 015.標準クラス/kagome3/src/kagome3/Kagome3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kagome3;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Kagome3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        
+       String str = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+str = str.replace("U", "う").replace("I", "い");
+System.out.print(str);
+        
+        
+    }}
